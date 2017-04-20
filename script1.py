6a5105aa8dc0d008a6331aeb89351a5ae3441ee7
D.M. Cook <dmaterialized@gmail.com>
2017-04-20 00:40:44 -0400

    solve v1, fix v2, begin calcs

--- script1.py
+++ script1.py
@@ -1,26 +1,34 @@
 from tkinter import *
 
-window=Tk()
+# ==== i n i t   w i n d o w =============================
+window=Tk() # init tkinter window
 
 # set up the functions
 def km_to_miles():
-    print(e1_value.get()) # value.get() lets us pull a value from Entry.
-    # insert a value to the text widget
-    miles=float(e1.value.get())*1.606 # didn't need to add float here, why not?
-    t1.insert(END,miles) # end enters new text at the end of any existing text.
+    print(float(e1_value.get())) # testing purposes only
+# ---- TODO: might have to remove float here ----
 
-b1 = Button(window,text="String Execute", command=km_to_miles)
+    # 1. value.get() lets us pull a value from Entry.
+    # 2. insert a value to the text widget
+    # 3. # END enters new text into a text area, but at the end of any existing text.
+    miles = float(e1_value.get())*1.6
+#   ^^ ||| the trouble spot ||| ^^
+#   edit: was referencing e1.value, not e1_value.
+#   lesson learned.
+    t1.insert(END,miles)
+
+b1 = Button(window,text="String Execute",command=km_to_miles)
 #  first, tell the button which window it's backed to.
 #  then, tell the text that should be used.
 #  command links it to a function.
-b1.pack() # apparently needed in order to create the button
-b1.grid(row=0,column=0) #
+b1.pack() # this is apparently needed in order to create the button
+b1.grid(row=0,column=0) # place the button in the upper-left corner.
 
 # grid is supposed to be better because you specify row/column.
 # we want a 2x5 grid layout.
 #
 # step 2 was to add line below:
-e1_value=StringVar() # this is to hold the string and be able to use get methods on it
+e1_value = StringVar() # this is to hold the string and be able to use get methods on it
 e1 = Entry(window,textvariable=e1_value) # --> textvariable=e1_value
 # create new button, specify window, don't specify label as it's a text area.
 e1.grid(row=0,column=1) # # place the button at these coords
@@ -32,3 +40,5 @@ t1.grid(row=0,column=2) #t1 is at these coords
 
 # ... ... ...
 window.mainloop() # required for all windows to draw.
+# ========================================================
+#   w i n d o w   1   e n d s
--- script2.py
+++ script2.py
@@ -8,27 +8,42 @@ window=Tk() # create the window
 
 # set up the functions
 
-
-def kg_to_pound():
-    print(e1_value.get()) #print whatever's in e1
-    pounds=e1_value.get()*2.20462 # convert to pounds
-    t1.insert(END,pounds) # in t1, insert that amount
-
 # ================
 # conversions:
 # 1 kg = 1000 grams
 # 1 kg = 2.20462 pounds
 # 1 kg = 35.274 ounces
 # ==================
+
+
+# convert to pounds
+def kg_to_pound():
+    print(e1_value.get()) # print whatever's in e1
+    pounds=float(e1_value.get())*2.20462 # convert to pounds
+    t1.insert(END,pounds) # in t1, insert that amount
+
+# convert to ounces
+# ...
+# convert to grams
+# ...
+
 e1_value=StringVar()
 
 # set up a 3 column (w) by 2 column (tall)
 
 # define buttons
 b1 = Button(window,textvariable=e1_value)
-    # grid
-t1=Text(window,height=1,width=20)
-t1.grid(row=0,column=2)
+b1.pack() #(why is this needed?)
+b1.grid(row=0,column=0) #place button here
+
+#e1 is an input of text that stores in Entry ()
+e1 = Entry(window,textvariable=e1_value)
+# e1.pack() isn't needed for non-buttons
+e1.grid(row=0,column=1)
+
+# t1 is going to record values
+t1=Text(window,height=1,width=10) # t1 is a text area on the right side
+t1.grid(row=1,column=0)
 
 
 
