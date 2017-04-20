c29d597bcc454c2034309dbde1e15ac7c4174cee
D.M. Cook <dmaterialized@gmail.com>
2017-04-20 01:12:44 -0400

    break completely to set up single step convert

--- script2.py
+++ script2.py
@@ -34,32 +34,43 @@ def kg_to_grams():
     grams=float(e1_value.get())*1000
     t1.insert(END,grams)
 
+
+def convert_from_kg():
+    gram=float(e2_value.get())*1000
+    pound=float(e2_value.get())*2.20462
+    ounce=float(e2_value.get())*35.274
+    t1.insert(END,gram)
+    t2.insert(END,pound)
+    t3.insert(END,ounce)
+
 e1_value=StringVar()
 
 # set up a 3 column (w) by 2 column (tall)
 #
 # define buttons
 # use text not textvariable here!
-b1 = Button(window,text="convert to pounds",command=kg_to_pound)
+
+b1 = Button(window,text="convert units",command=convert_from_kg)
+#b1 = Button(window,text="convert to pounds",command=kg_to_pound)
 b1.pack() # (why is this needed?)
-b1.grid(row=1,column=0) # place button here
+b1.grid(row=0,column=0) # place button here
 
-b2=Button(window,text="convert to ounces",command=kg_to_oz)
-b2.pack()
-b2.grid(row=1,column=1)
+# b2=Button(window,text="convert to ounces",command=kg_to_oz)
+# b2.pack()
+# b2.grid(row=1,column=1)
 
-b3=Button(window,text="convert to grams",command=kg_to_grams)
-b3.pack()
-b3.grid(row=1,column=2)
+# b3=Button(window,text="convert to grams",command=kg_to_grams)
+# b3.pack()
+# b3.grid(row=1,column=2)
 
 # e1 is an input of text that stores in Entry ()
 e1 = Entry(window,textvariable=e1_value)
 # e1.pack() isn't needed for non-buttons
-e1.grid(row=0,column=0)
+e1.grid(row=0,column=1)
 
 # t1 is going to record values
 t1=Text(window,height=1,width=10) # t1 is a text area on the right side
-t1.grid(row=0,column=2)
+t1.grid(row=0,column=1)
 
 # how to set up a label?
 # l1=Label("kg")
